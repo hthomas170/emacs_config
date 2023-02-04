bc062381e812b8b6a2ac328e0d216faae37eab3d
Hugo Thomas <hugo.thomas170@protonmail.com>
2023-02-04 11:23:02 +0100

    ADD: reference to custom.el file in init.el

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "~/.guix-profile/share/emacs/site-lisp")
 (guix-emacs-autoload-packages)
 (org-babel-load-file (expand-file-name "config.org" "~/.emacs.d"))
-
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file))
